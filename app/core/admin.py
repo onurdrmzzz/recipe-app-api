8e799542c739f5d06d1eda23906c7460f737a021
onurdrmzzz <onurdrmzzz@gmail.com>
2022-09-13 03:52:56 +0300

    Customize admin page

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,4 +1,30 @@
-from django.contrib import admin # noqa
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from core import models
 
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        )
+    )
+
+
+admin.site.register(models.User, UserAdmin)

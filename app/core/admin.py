53c77404d637a78f66da303d92ae7a191ac1dd31
onurdrmzzz <onurdrmzzz@gmail.com>
2022-09-13 04:34:54 +0300

    customized admin page

--- app/core/admin.py
+++ app/core/admin.py
@@ -20,10 +20,26 @@ class UserAdmin(BaseUserAdmin):
                 'fields': (
                     'is_active',
                     'is_staff',
-                    'is_superuser'
+                    'is_superuser',
                 )
             }
-        )
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
     )
 
 
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -7,11 +7,11 @@ from django.urls import reverse
 from django.test import Client
 
 
-class AdminSiteTestCase(TestCase):
+class AdminSiteTests(TestCase):
     """Tests for Django admin."""
 
     def setUp(self):
-        """Create user and client"""
+        """Create user and client."""
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
             email='admin@example.com',
@@ -24,7 +24,7 @@ class AdminSiteTestCase(TestCase):
             name='Test User'
         )
 
-    def test_users_list(self):
+    def test_users_lists(self):
         """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
@@ -39,3 +39,9 @@ class AdminSiteTestCase(TestCase):
 
         self.assertEqual(res.status_code, 200)
 
+    def test_create_user_page(self):
+        """Test the create user page works."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
